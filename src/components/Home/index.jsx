bb2e3d05c6c3410c1400babda8d3d50b43281b54
wilder <Paco_fleuru@yahoo.fr>
2022-02-03 14:26:33 +0100

    Git init front

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="PageMain BackgroundImg">
+      <title>Home</title>
+      <div className="BoxTitreMain">
+      <h1 className="TitreMain">World of $Huriken</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
